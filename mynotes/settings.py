9585a78aa7e190a51ac0adf162a7d395216a11c1
gerardonastri <90633465+gerardonastri@users.noreply.github.com>
2022-06-12 09:20:40 +0200

    better

--- mynotes/settings.py
+++ mynotes/settings.py
@@ -142,11 +142,12 @@ STATICFILES_DIRS = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     "https://notes-one-orpin.vercel.app"
 ]  # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
 CORS_ALLOWED_ORIGIN_REGEXES = [
     'http://localhost:3000',
-    "https://notes-one-orpin.vercel.app/"
+    "https://notes-one-orpin.vercel.app"
 ]
